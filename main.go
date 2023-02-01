5318c3a475cc14a3ab8802162cca70f0a926cae2
aeff60 <ssbunklang@gmail.com>
2023-02-01 20:53:39 +0700

    initail commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package cryptdecrypt2
+
+func Encrypt(text string, key string) string {
+	data := []byte(text)
+	for i := range data {
+		data[i] ^= key[i%len(key)]
+	}
+	return string(data)
+}
+
+func Decrypt_text(text string, key string) string {
+	return Encrypt(text, key)
+}
